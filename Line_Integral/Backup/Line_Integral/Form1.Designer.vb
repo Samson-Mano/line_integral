bd8bd73a4efa915d3a72adfa7e7b39f6f6e1608a
Samson-Mano <60636238+Samson-Mano@users.noreply.github.com>
2020-06-27 01:33:30 -0400

    Add files via upload

---
+++ Line_Integral/Backup/Line_Integral/Form1.Designer.vb
@@ -0,0 +1,170 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Main_Pic = New System.Windows.Forms.Panel
+        Me.MT_Pic = New System.Windows.Forms.PictureBox
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip
+        Me.ControlToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem
+        Me.StatusStrip1 = New System.Windows.Forms.StatusStrip
+        Me.Label1 = New System.Windows.Forms.Label
+        Me.TextBox_ZVal = New System.Windows.Forms.TextBox
+        Me.Button_SetVals = New System.Windows.Forms.Button
+        Me.Label_ZVal = New System.Windows.Forms.Label
+        Me.ComboBox_ZVal = New System.Windows.Forms.ComboBox
+        Me.Main_Pic.SuspendLayout()
+        CType(Me.MT_Pic, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Main_Pic
+        '
+        Me.Main_Pic.BackColor = System.Drawing.Color.White
+        Me.Main_Pic.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.Main_Pic.Controls.Add(Me.MT_Pic)
+        Me.Main_Pic.Location = New System.Drawing.Point(13, 28)
+        Me.Main_Pic.Margin = New System.Windows.Forms.Padding(4)
+        Me.Main_Pic.Name = "Main_Pic"
+        Me.Main_Pic.Size = New System.Drawing.Size(800, 850)
+        Me.Main_Pic.TabIndex = 0
+        '
+        'MT_Pic
+        '
+        Me.MT_Pic.BackColor = System.Drawing.Color.Transparent
+        Me.MT_Pic.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.MT_Pic.Enabled = False
+        Me.MT_Pic.Location = New System.Drawing.Point(0, 0)
+        Me.MT_Pic.Name = "MT_Pic"
+        Me.MT_Pic.Size = New System.Drawing.Size(796, 846)
+        Me.MT_Pic.TabIndex = 0
+        Me.MT_Pic.TabStop = False
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ControlToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Padding = New System.Windows.Forms.Padding(8, 2, 0, 2)
+        Me.MenuStrip1.Size = New System.Drawing.Size(1184, 24)
+        Me.MenuStrip1.TabIndex = 1
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'ControlToolStripMenuItem
+        '
+        Me.ControlToolStripMenuItem.Name = "ControlToolStripMenuItem"
+        Me.ControlToolStripMenuItem.Size = New System.Drawing.Size(59, 20)
+        Me.ControlToolStripMenuItem.Text = "Control"
+        '
+        'StatusStrip1
+        '
+        Me.StatusStrip1.Location = New System.Drawing.Point(0, 890)
+        Me.StatusStrip1.Name = "StatusStrip1"
+        Me.StatusStrip1.Size = New System.Drawing.Size(1184, 22)
+        Me.StatusStrip1.TabIndex = 2
+        Me.StatusStrip1.Text = "StatusStrip1"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(829, 59)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(31, 16)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Z ="
+        '
+        'TextBox_ZVal
+        '
+        Me.TextBox_ZVal.Font = New System.Drawing.Font("Verdana", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.TextBox_ZVal.Location = New System.Drawing.Point(866, 56)
+        Me.TextBox_ZVal.Multiline = True
+        Me.TextBox_ZVal.Name = "TextBox_ZVal"
+        Me.TextBox_ZVal.Size = New System.Drawing.Size(306, 132)
+        Me.TextBox_ZVal.TabIndex = 4
+        '
+        'Button_SetVals
+        '
+        Me.Button_SetVals.Location = New System.Drawing.Point(910, 194)
+        Me.Button_SetVals.Name = "Button_SetVals"
+        Me.Button_SetVals.Size = New System.Drawing.Size(211, 54)
+        Me.Button_SetVals.TabIndex = 5
+        Me.Button_SetVals.Text = "Generate Z Function"
+        Me.Button_SetVals.UseVisualStyleBackColor = True
+        '
+        'Label_ZVal
+        '
+        Me.Label_ZVal.AutoSize = True
+        Me.Label_ZVal.Font = New System.Drawing.Font("Verdana", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label_ZVal.Location = New System.Drawing.Point(829, 302)
+        Me.Label_ZVal.Name = "Label_ZVal"
+        Me.Label_ZVal.Size = New System.Drawing.Size(0, 18)
+        Me.Label_ZVal.TabIndex = 6
+        '
+        'ComboBox_ZVal
+        '
+        Me.ComboBox_ZVal.FormattingEnabled = True
+        Me.ComboBox_ZVal.Items.AddRange(New Object() {"1", "0.1", "0.01", "0.001", "0.0001"})
+        Me.ComboBox_ZVal.Location = New System.Drawing.Point(1034, 296)
+        Me.ComboBox_ZVal.Name = "ComboBox_ZVal"
+        Me.ComboBox_ZVal.Size = New System.Drawing.Size(138, 24)
+        Me.ComboBox_ZVal.TabIndex = 7
+        Me.ComboBox_ZVal.Text = "0.1"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1184, 912)
+        Me.Controls.Add(Me.ComboBox_ZVal)
+        Me.Controls.Add(Me.Label_ZVal)
+        Me.Controls.Add(Me.Button_SetVals)
+        Me.Controls.Add(Me.TextBox_ZVal)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.StatusStrip1)
+        Me.Controls.Add(Me.Main_Pic)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.Font = New System.Drawing.Font("Verdana", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Margin = New System.Windows.Forms.Padding(4)
+        Me.Name = "Form1"
+        Me.Text = "Line Integral Visualization"
+        Me.Main_Pic.ResumeLayout(False)
+        CType(Me.MT_Pic, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Main_Pic As System.Windows.Forms.Panel
+    Friend WithEvents MenuStrip1 As System.Windows.Forms.MenuStrip
+    Friend WithEvents ControlToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents MT_Pic As System.Windows.Forms.PictureBox
+    Friend WithEvents StatusStrip1 As System.Windows.Forms.StatusStrip
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents TextBox_ZVal As System.Windows.Forms.TextBox
+    Friend WithEvents Button_SetVals As System.Windows.Forms.Button
+    Friend WithEvents Label_ZVal As System.Windows.Forms.Label
+    Friend WithEvents ComboBox_ZVal As System.Windows.Forms.ComboBox
+
+End Class
---
+++ Line_Integral/Line_Integral/Form1.vb
@@ -0,0 +1,399 @@
+﻿Public Class Form1
+    Public Shared ScaleVal As Integer = 4000
+    Public Shared Coord_Scale As Double = 0.01
+    Const MidPtX As Integer = 400
+    Const MidPtY As Integer = 400
+    Const XNExt As Integer = -8
+    Const XPExt As Integer = 9
+    Const YNExt As Integer = -10
+    Const YPExt As Integer = 11
+    Const CntrInterval As Integer = 10
+    Dim _ScV As New List(Of Surface_Holder)
+    Dim _meshTri As New List(Of Mesh_Holder)
+
+    Public Shared Function Z_Function(ByVal _XV As Double, _
+                               ByVal _YV As Double) As Double
+        Dim _ZVal As Double
+        '------ Test function to set Z-Values
+        Dim TsFunc As Integer = 3
+        If TsFunc = 1 Then
+            'test function to give rings above a certain level
+            '_ZVal = (4 / (1 + (_XV * _XV) + (_YV * _YV))) - (3 / (1 + ((_XV * _XV) + (_YV * _YV)) * ((_XV * _XV) + (_YV * _YV))))
+            _ZVal = Math.Sin(_XV * _YV)
+        ElseIf TsFunc = 2 Then
+            'test function to give an apparent convecting and diffusing source
+            If Math.Abs(_YV) < 0.125 Then
+                _ZVal = Math.Exp(-_XV) + (0.05 * (4 - _XV))
+            Else
+                _ZVal = Math.Exp(-_XV) * (1 - Math.Log(8 * Math.Abs(_YV))) / Math.Log(8) + (0.05 * (4 - _XV))
+            End If
+        ElseIf TsFunc = 3 Then
+            'test function to give modulated sinusoidal dependance
+            _ZVal = (Math.Cos(4 * _XV) / (1 + (_XV * _XV)) + Math.Sin(4 * _YV) / (1 + (_YV * _YV))) / 2
+        End If
+        Return _ZVal
+    End Function
+
+    Public Class Surface_Holder
+        Dim _VXCoord As Double
+        Dim _VYCoord As Double
+        Dim _VXlevel As Integer
+        Dim _VYlevel As Integer
+        Dim _ScXCoord As Integer
+        Dim _ScYCoord As Integer
+        Dim _ZVal As Double
+
+        Public ReadOnly Property VXCoord() As Double
+            Get
+                Return _VXCoord
+            End Get
+        End Property
+
+        Public ReadOnly Property VYCoord() As Double
+            Get
+                Return _VYCoord
+            End Get
+        End Property
+
+        Public ReadOnly Property VXlevel() As Integer
+            Get
+                Return _VXlevel
+            End Get
+        End Property
+
+        Public ReadOnly Property VYlevel() As Integer
+            Get
+                Return _VYlevel
+            End Get
+        End Property
+
+        Public ReadOnly Property ScXCoord() As Integer
+            Get
+                Return _ScXCoord
+            End Get
+        End Property
+
+        Public ReadOnly Property ScYCoord() As Integer
+            Get
+                Return _ScYCoord
+            End Get
+        End Property
+
+        Public ReadOnly Property ZVal() As Double
+            Get
+                Return _ZVal
+            End Get
+        End Property
+
+        Public Sub Set_DCoordinates(ByVal _XV As Double, _
+                                    ByVal _YV As Double, _
+                                    ByVal _Xlvl As Integer, _
+                                    ByVal _Ylvl As Integer)
+            _VXCoord = _XV
+            _VYCoord = _YV
+
+            _VXlevel = _Xlvl
+            _VYlevel = _Ylvl
+
+            '------ Set the display x,y coordinates
+            _ScXCoord = CInt(ScaleVal * _XV) + MidPtX
+            _ScYCoord = MidPtY - CInt(ScaleVal * _YV) '--- inverse coordinate TOP down to Top up
+
+            '------ Test function to set Z-Values
+            _ZVal = Z_Function(_XV, _YV)
+
+        End Sub
+
+    End Class
+
+    Public Class IntersectionCurve_Holder
+        Dim _SPointX As Integer
+        Dim _SPointY As Integer
+        Dim _EPointX As Integer
+        Dim _EPointY As Integer
+        Dim _SPX As Double
+        Dim _SPY As Double
+        Dim _EPX As Double
+        Dim _EPY As Double
+        Dim _ZCntrLvl As Double
+
+        Public ReadOnly Property SPointX() As Integer
+            Get
+                Return _SPointX
+            End Get
+        End Property
+
+        Public ReadOnly Property SPointY() As Integer
+            Get
+                Return _SPointY
+            End Get
+        End Property
+
+        Public ReadOnly Property EPointX() As Integer
+            Get
+                Return _EPointX
+            End Get
+        End Property
+
+        Public ReadOnly Property EPointY() As Integer
+            Get
+                Return _EPointY
+            End Get
+        End Property
+
+        Public Sub Set_CurvePoints(ByVal t_SPX As Double, _
+                                   ByVal t_SPY As Double, _
+                                   ByVal t_EPX As Double, _
+                                   ByVal t_EPY As Double, _
+                                   ByVal t_ZcntrLvl As Double)
+
+            _SPX = t_SPX
+            _SPY = t_SPY
+            _EPX = t_EPX
+            _EPY = t_EPY
+            _ZCntrLvl = t_ZcntrLvl
+
+            '------ Set the display x,y coordinates - one End
+            _SPointX = CInt(ScaleVal * t_SPX) + MidPtX
+            _SPointY = MidPtY - CInt(ScaleVal * t_SPY) '--- inverse coordinate TOP down to Top up
+
+            '------ Set the display x,y coordinates - Second End
+            _EPointX = CInt(ScaleVal * t_EPX) + MidPtX
+            _EPointY = MidPtY - CInt(ScaleVal * t_EPY) '--- inverse coordinate TOP down to Top up
+        End Sub
+    End Class
+
+    Public Class Mesh_Holder
+        Dim _scPt1 As New Surface_Holder
+        Dim _scPt2 As New Surface_Holder
+        Dim _scPt3 As New Surface_Holder
+        Dim ZVal1 As Double
+        Dim ZVal2 As Double
+        Dim ZVal3 As Double
+        Dim _isecCurve As New List(Of IntersectionCurve_Holder)
+
+        Public ReadOnly Property scPt1() As Surface_Holder
+            Get
+                Return _scPt1
+            End Get
+        End Property
+
+        Public ReadOnly Property scPt2() As Surface_Holder
+            Get
+                Return _scPt2
+            End Get
+        End Property
+
+        Public ReadOnly Property scPt3() As Surface_Holder
+            Get
+                Return _scPt3
+            End Get
+        End Property
+
+        Public ReadOnly Property isecCurve() As List(Of IntersectionCurve_Holder)
+            Get
+                Return _isecCurve
+            End Get
+        End Property
+
+        Public Sub set_Values(ByVal temp_ScPt1 As Surface_Holder, _
+                              ByVal temp_ScPt2 As Surface_Holder, _
+                              ByVal temp_ScPt3 As Surface_Holder)
+            '----------- Just fix the three coordinates and Z values of the trianlge mesh
+            _scPt1 = temp_ScPt1
+            _scPt2 = temp_ScPt2
+            _scPt3 = temp_ScPt3
+
+            ZVal1 = temp_ScPt1.ZVal
+            ZVal2 = temp_ScPt2.ZVal
+            ZVal3 = temp_ScPt3.ZVal
+
+            _isecCurve = New List(Of IntersectionCurve_Holder)
+        End Sub
+
+        Public Sub set_iseccurve(ByVal Cntr_Val As Double)
+            Dim ZSlope1 As Double
+            Dim ZSlope2 As Double
+            Dim temp_isecCurve As New IntersectionCurve_Holder
+            Dim _SPX As Double
+            Dim _SPY As Double
+            Dim _EPX As Double
+            Dim _EPY As Double
+
+            If (ZVal1 - Cntr_Val) * (ZVal2 - Cntr_Val) < 0 Then
+                '--- Triangle with Zval1 or Zval 2 is below the contour range
+                If (ZVal2 - Cntr_Val) * (ZVal3 - Cntr_Val) < 0 Then
+                    '--- Triangle with Zval2 is above or below the contour range
+                    ZSlope1 = (Cntr_Val - ZVal2) / (ZVal1 - ZVal2) '---2 to 1
+                    _SPX = scPt2.VXCoord * (1 - ZSlope1) + scPt1.VXCoord * ZSlope1
+                    _SPY = scPt2.VYCoord * (1 - ZSlope1) + scPt1.VYCoord * ZSlope1
+
+                    ZSlope2 = (Cntr_Val - ZVal2) / (ZVal3 - ZVal2) '---2 to 3
+                    _EPX = scPt2.VXCoord * (1 - ZSlope2) + scPt3.VXCoord * ZSlope2
+                    _EPY = scPt2.VYCoord * (1 - ZSlope2) + scPt3.VYCoord * ZSlope2
+
+                    temp_isecCurve.Set_CurvePoints(_SPX, _SPY, _EPX, _EPY, Cntr_Val)
+                ElseIf (ZVal1 - Cntr_Val) * (ZVal3 - Cntr_Val) < 0 Then
+                    '--- Triangle with Zval1 is above or below the contour range
+                    ZSlope1 = (Cntr_Val - ZVal1) / (ZVal2 - ZVal1) '---1 to 2
+                    _SPX = scPt1.VXCoord * (1 - ZSlope1) + scPt2.VXCoord * ZSlope1
+                    _SPY = scPt1.VYCoord * (1 - ZSlope1) + scPt2.VYCoord * ZSlope1
+
+                    ZSlope2 = (Cntr_Val - ZVal1) / (ZVal3 - ZVal1) '---1 to 3
+                    _EPX = scPt1.VXCoord * (1 - ZSlope2) + scPt3.VXCoord * ZSlope2
+                    _EPY = scPt1.VYCoord * (1 - ZSlope2) + scPt3.VYCoord * ZSlope2
+
+                    temp_isecCurve.Set_CurvePoints(_SPX, _SPY, _EPX, _EPY, Cntr_Val)
+                End If
+            ElseIf (ZVal2 - Cntr_Val) * (ZVal3 - Cntr_Val) < 0 Then
+                If (ZVal1 - Cntr_Val) * (ZVal3 - Cntr_Val) < 0 Then
+                    '--- Triangle with Zval 3 is above or below  the contour range
+                    ZSlope1 = (Cntr_Val - ZVal3) / (ZVal1 - ZVal3) '---3 to 1
+                    _SPX = scPt3.VXCoord * (1 - ZSlope1) + scPt1.VXCoord * ZSlope1
+                    _SPY = scPt3.VYCoord * (1 - ZSlope1) + scPt1.VYCoord * ZSlope1
+
+                    ZSlope2 = (Cntr_Val - ZVal3) / (ZVal2 - ZVal3) '---3 to 2
+                    _EPX = scPt3.VXCoord * (1 - ZSlope2) + scPt2.VXCoord * ZSlope2
+                    _EPY = scPt3.VYCoord * (1 - ZSlope2) + scPt2.VYCoord * ZSlope2
+
+                    temp_isecCurve.Set_CurvePoints(_SPX, _SPY, _EPX, _EPY, Cntr_Val)
+                End If
+            End If
+            _isecCurve.Add(temp_isecCurve)
+        End Sub
+    End Class
+
+#Region " Main Pic Events"
+    Private Sub Main_Pic_Paint(ByVal sender As Object, ByVal e As System.Windows.Forms.PaintEventArgs) Handles Main_Pic.Paint
+        e.Graphics.DrawLine(Pens.Blue, MidPtX, 0, MidPtX, 900)
+        e.Graphics.DrawLine(Pens.Blue, 0, MidPtY, 800, MidPtY)
+        'Dim i, j As Integer
+
+        '----------- Coordinates
+        'For Each Sc In _ScV
+        '    e.Graphics.DrawEllipse(Pens.Blue, Sc.ScXCoord - CInt(ScaleVal * 0.5), Sc.ScYCoord - CInt(ScaleVal * 0.5), ScaleVal, ScaleVal)
+        'Next
+
+        '----------- Mesh_Triangle
+        For Each M_T In _meshTri
+            e.Graphics.DrawLine(Pens.Blue, M_T.scPt1.ScXCoord, M_T.scPt1.ScYCoord, _
+                                           M_T.scPt2.ScXCoord, M_T.scPt2.ScYCoord)
+            e.Graphics.DrawLine(Pens.Blue, M_T.scPt2.ScXCoord, M_T.scPt2.ScYCoord, _
+                                          M_T.scPt3.ScXCoord, M_T.scPt3.ScYCoord)
+            e.Graphics.DrawLine(Pens.Blue, M_T.scPt3.ScXCoord, M_T.scPt3.ScYCoord, _
+                                          M_T.scPt1.ScXCoord, M_T.scPt1.ScYCoord)
+            For Each int_Sec In M_T.isecCurve
+                e.Graphics.DrawLine(Pens.Black, int_Sec.SPointX, int_Sec.SPointY, _
+                                               int_Sec.EPointX, int_Sec.EPointY)
+            Next
+        Next
+
+        '---- Pain Zoom Status
+        e.Graphics.DrawString(Coord_Scale, New Font("Verdana", 12), Brushes.Blue, 700, 800)
+    End Sub
+#End Region
+
+
+    Private Sub Form1_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+      
+    End Sub
+
+    Private Sub Button_SetVals_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button_SetVals.Click
+        Execute_ZFunction()
+    End Sub
+
+    Public Sub Execute_ZFunction()
+        Dim i, j As Integer
+
+        '----------- Set Z_Function with the text values
+
+
+
+        '-------- Fix X, Y, Z_Values for Surface
+        _ScV = New List(Of Surface_Holder)
+        For i = XNExt To XPExt Step +1
+            For j = YNExt To YPExt Step +1
+                Dim temp_ScV As New Surface_Holder
+                Dim temp_X As Double
+                Dim temp_Y As Double
+                temp_X = Coord_Scale * ((i - 1) + ((Math.Abs(j) + 2) Mod 2) * 0.5)
+                temp_Y = Coord_Scale * ((j - 1) * Math.Sqrt(3) * 0.5)
+                temp_ScV.Set_DCoordinates(temp_X, temp_Y, i, j)
+                _ScV.Add(temp_ScV)
+            Next
+        Next
+
+        '-------- Fix Triangle Mesh
+        _meshTri = New List(Of Mesh_Holder)
+        For i = XNExt To (XPExt - 1) Step +1
+            Dim k0 As Integer = i
+            Dim Sc_tep1 As New List(Of Surface_Holder)
+            Sc_tep1 = _ScV.Where(Function(c) c.VXlevel = k0 + 0).ToList
+            Dim Sc_tep2 As New List(Of Surface_Holder)
+            Sc_tep2 = _ScV.Where(Function(c) c.VXlevel = k0 + 1).ToList
+            For j = YNExt To (YPExt - 1) Step +1
+                Dim k1 As Integer = j
+                Dim temp_MeshTri As Mesh_Holder
+
+                '--------- Triangle
+                Dim op1 As Integer = If((Math.Abs(j)) Mod 2 = 0, j, j + 1)
+                Dim ScPt1, ScPt2, ScPt3 As Surface_Holder
+                ScPt1 = Sc_tep1.Find(Function(c) c.VYlevel = k1)
+                ScPt2 = Sc_tep1.Find(Function(c) c.VYlevel = (k1 + 1))
+                ScPt3 = Sc_tep2.Find(Function(c) c.VYlevel = (op1))
+                temp_MeshTri = New Mesh_Holder
+                temp_MeshTri.set_Values(ScPt1, ScPt2, ScPt3)
+                _meshTri.Add(temp_MeshTri)
+
+                '---------- Inverse Triangle
+                Dim op2 As Integer = If((Math.Abs(j)) Mod 2 = 0, j + 1, j)
+                ScPt1 = Sc_tep2.Find(Function(c) c.VYlevel = k1)
+                ScPt2 = Sc_tep2.Find(Function(c) c.VYlevel = (k1 + 1))
+                ScPt3 = Sc_tep1.Find(Function(c) c.VYlevel = (op2))
+                temp_MeshTri = New Mesh_Holder
+                temp_MeshTri.set_Values(ScPt1, ScPt2, ScPt3)
+                _meshTri.Add(temp_MeshTri)
+            Next
+        Next
+
+        '-------- Find the contour intersection
+        Dim ZNExt, ZPExt As Double
+        ZNExt = 100000
+        ZPExt = -100000
+        For Each _Sc In _ScV
+            If ZNExt > _Sc.ZVal Then
+                ZNExt = _Sc.ZVal
+            End If
+            If ZPExt < _Sc.ZVal Then
+                ZPExt = _Sc.ZVal
+            End If
+        Next
+        Dim cntrLvl As Double
+        For i = 1 To (CntrInterval - 1) Step +1
+            cntrLvl = ZNExt + ((ZPExt - ZNExt) / CntrInterval) * i
+            '-------- Set contour level
+            For Each _MeshT In _meshTri
+                _MeshT.set_iseccurve(cntrLvl)
+            Next
+        Next
+
+        MT_Pic.Refresh()
+    End Sub
+
+    Private Sub ComboBox_ZVal_SelectedIndexChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles ComboBox_ZVal.SelectedIndexChanged
+        If ComboBox_ZVal.SelectedIndex = 0 Then
+            ScaleVal = 40
+            Coord_Scale = 1
+        ElseIf ComboBox_ZVal.SelectedIndex = 1 Then
+            ScaleVal = 400
+            Coord_Scale = 0.1
+        ElseIf ComboBox_ZVal.SelectedIndex = 2 Then
+            ScaleVal = 4000
+            Coord_Scale = 0.01
+        ElseIf ComboBox_ZVal.SelectedIndex = 3 Then
+            ScaleVal = 40000
+            Coord_Scale = 0.001
+        End If
+        Execute_ZFunction()
+    End Sub
+End Class
